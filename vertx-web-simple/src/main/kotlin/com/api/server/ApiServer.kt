ba596a0de3f7ee77143786aff35072b8761cec1f
龙明 <dragonsunmoon@gmail.com>
2019-03-29 16:57:08 +0800

    add project template: vertx-web-simple

---
+++ vertx-web-simple/src/main/kotlin/com/api/server/ApiServer.kt
@@ -0,0 +1,25 @@
+package com.api.server
+
+import sz.SzEbeanConfig
+import sz.scaffold.Application
+
+//
+// Created by kk on 17/8/29.
+//
+object ApiServer {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        SzEbeanConfig.loadConfig()
+
+        Application.setupVertx()
+
+//        Application.regOnStartHandler(50) {
+//
+//        }
+
+        Application.runHttpServer()
+
+    }
+
+}
