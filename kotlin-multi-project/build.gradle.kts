73d1082f9b869e8197c212ea3931de967ba35a30
龙明 <dragonsunmoon@gmail.com>
2019-06-21 13:37:32 +0800

    upgrade kotlin to 1.3.40

--- kotlin-multi-project/build.gradle.kts
+++ kotlin-multi-project/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    id("org.jetbrains.kotlin.jvm").version("1.3.31").apply(false)
+    id("org.jetbrains.kotlin.jvm").version("1.3.40").apply(false)
 }
 
 allprojects {
