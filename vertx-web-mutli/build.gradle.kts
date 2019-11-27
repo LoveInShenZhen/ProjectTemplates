95ff87bff8a132e655749a67feabb7d3aa9682c1
龙明 <dragonsunmoon@gmail.com>
2019-11-28 00:18:34 +0800

    upgrade kotlin to 1.3.61

--- vertx-web-mutli/build.gradle.kts
+++ vertx-web-mutli/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    id("org.jetbrains.kotlin.jvm").version("1.3.50").apply(false)
+    id("org.jetbrains.kotlin.jvm").version("1.3.61").apply(false)
 }
 
 allprojects {
