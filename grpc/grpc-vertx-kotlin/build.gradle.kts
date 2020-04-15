fa16bc56438761d65c6ef850a1102a2b341ccbce
IT写轮眼 <kklongmono@gmail.com>
2020-04-15 20:32:05 +0800

    upgrade kotlin to 1.3.72

--- grpc/grpc-vertx-kotlin/build.gradle.kts
+++ grpc/grpc-vertx-kotlin/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    id("org.jetbrains.kotlin.jvm").version("1.3.71").apply(false)
+    id("org.jetbrains.kotlin.jvm").version("1.3.72").apply(false)
 }
 
 allprojects {
